265b784447815a8cf55fa84ab3e0fa0e892d9416
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-05-26 23:48:21 +0500

    feat: create main.py
    Note: since the DB and environment variables are not created, the runserver will crash with an error.
    Also in the future it is necessary to remove the comment type: # ignore[unused-ignore].

---
+++ cheatsheet/main.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from database.database_helper import db_helper
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # FastAPI is initialized, but does not accept requests yet.
+    # Here you can, for example, check the connection to the database.
+    yield
+    # Ensures that all database connections are closed gracefully
+    # when the application terminates
+    await db_helper.dispose()
+
+
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)  # type: ignore[unused-ignore]
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        reload=True,
+    )
