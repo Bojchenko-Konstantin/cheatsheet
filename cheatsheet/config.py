3848acc122b63966d5ee000917cd3098565b7a04
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-05-31 22:34:07 +0500

    fix: fixed naming_convention

--- cheatsheet/config.py
+++ cheatsheet/config.py
@@ -14,11 +14,11 @@ class DatabaseConfig(BaseModel):
     max_overflow: int = 10
 
     naming_convention: dict[str, str] = {
-        "ix": "ix_%(column_0_label)",
-        "uq": "uq_%(table_name)_%(column_0_N_name)",
-        "ck": "ck_%(table_name)_%(constraint_name)",
-        "fk": "fk_%(table_name)_%(column_0_name)s_%(referred_table_name)",
-        "pk": "pk_%(table_name)",
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
     }
 
 
@@ -28,7 +28,7 @@ class LoggingConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=(f"{BASE_DIR}/.env.template", f"{BASE_DIR}/.env"),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
