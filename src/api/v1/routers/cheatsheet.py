44af48a33eba54cbadcb301364ca056b277340b8
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-07-31 18:08:57 +0400

    fix: imports, dependency

--- src/api/v1/routers/cheatsheet.py
+++ src/api/v1/routers/cheatsheet.py
@@ -1,8 +1,10 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
 
-from api.v1.dependencies import get_cheatsheet_use_case
-from application.dto.schemas import CheatsheetRead
-from application.use_cases.cheatsheet import CheatsheetUseCase
+from src.api.v1.dependencies import get_cheatsheet_use_case
+from src.application.dto.schemas import CheatsheetRead
+from src.application.use_cases.cheatsheet import CheatsheetUseCase
 
 router = APIRouter(prefix="/cheatsheets", tags=["Cheatsheets"])
 
@@ -10,7 +12,7 @@ router = APIRouter(prefix="/cheatsheets", tags=["Cheatsheets"])
 @router.get("/{cheatsheet_id}", response_model=CheatsheetRead)
 async def get_cheatsheet_by_id(
     cheatsheet_id: int,
-    use_case: CheatsheetUseCase = Depends(get_cheatsheet_use_case),
+    use_case: Annotated[CheatsheetUseCase, Depends(get_cheatsheet_use_case)],
 ):
     cheatsheet = await use_case.get_cheatsheet_by_id(cheatsheet_id)
     if not cheatsheet:
