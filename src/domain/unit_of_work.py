15b76e41338672229fecb73421b28f978dcbfa14
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-08-02 23:26:28 +0400

    fix: imports in domain/unit_of_work

--- src/domain/unit_of_work.py
+++ src/domain/unit_of_work.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any, Self
 
-from domain.repositories.cheatsheet import ICheatsheetRepo
+from src.domain.repositories.cheatsheet import ICheatsheetRepo
 
 
 class UnitOfWork(ABC):
