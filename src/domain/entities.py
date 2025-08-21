18bc4f4b9ca5c0e080f5b7392df40bf30e68c8c3
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-08-21 16:13:23 +0500

    fix: make entities mutable

--- src/domain/entities.py
+++ src/domain/entities.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class Tag:
     tag_id: int
     tag_name: str
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class Cheatsheet:
     cheatsheet_id: int
     title: str
