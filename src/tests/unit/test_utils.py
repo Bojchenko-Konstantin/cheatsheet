d8a7856d3bedd229e89c4da018f23b8d9fb83268
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-08-25 12:28:50 +0400

    fix: refactor test

--- src/tests/unit/test_utils.py
+++ src/tests/unit/test_utils.py
@@ -2,23 +2,14 @@ import pytest
 
 from src.core.utils import convert_model_class_to_table_name
 
-EXPECTED_RESULTS = [
-    (0, "http_request"),
-    (1, "fast_api"),
-    (2, "cheatsheet_to_tag"),
-    (3, "http_reques2t"),
+TEST_CASES = [
+    ("HTTPRequestModel", "http_request"),
+    ("FastAPI", "fast_api"),
+    ("CheatsheetToTagModel", "cheatsheet_to_tag"),
+    ("HTTPReques2t", "http_reques2t"),
 ]
 
 
-@pytest.mark.parametrize("index, expected_name", EXPECTED_RESULTS)
-def test_convert_model_class_to_table_name(index: int, expected_name: str):
-    results = [
-        convert_model_class_to_table_name(model_name)
-        for model_name in [
-            "HTTPRequestModel",
-            "FastAPI",
-            "CheatsheetToTagModel",
-            "HTTPReques2t",
-        ]
-    ]
-    assert results[index] == expected_name
+@pytest.mark.parametrize("input_name, expected_result", TEST_CASES)
+def test_convert_model_class_to_table_name(input_name: str, expected_result: str):
+    assert convert_model_class_to_table_name(input_name) == expected_result
