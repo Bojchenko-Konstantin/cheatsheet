d74d1f76642ad59b7a5cb2e934e3f2d957bf63be
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-10-22 22:37:24 +0500

    test: Add the test_create_cheatsheet_success to the cheatsheet use case

--- src/tests/unit/test_use_cases.py
+++ src/tests/unit/test_use_cases.py
@@ -25,7 +25,7 @@ class FakeCheatsheetRepo(ICheatsheetRepo):
         )
 
     async def create(self, cheatsheet: Cheatsheet) -> Cheatsheet:
-        pass
+        return cheatsheet
 
 
 class FakeUnitOfWork(IUnitOfWork):
@@ -61,3 +61,34 @@ class TestCheatsheetUseCase:
         cheatsheet = await sut.get_by_id(existing_id)
 
         assert cheatsheet == expected_result
+
+    async def test_create_cheatsheet_success(self):
+        unit_of_work = FakeUnitOfWork()
+        sut = CheatsheetUseCase(unit_of_work)
+        creation_data = Cheatsheet(
+            cheatsheet_id=UUID("01998b2f-af53-7ca0-85f3-9c01093dd430"),
+            title="title",
+            content="content",
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+            is_public=True,
+            tags={Tag(1, "Python"), Tag(2, "Testing")},
+            count_like=0,
+            count_view=0,
+        )
+
+        created_cheatsheet = await sut.create(creation_data)
+
+        expected_result = Cheatsheet(
+            cheatsheet_id=created_cheatsheet.cheatsheet_id,
+            title=creation_data.title,
+            content=creation_data.content,
+            created_at=created_cheatsheet.created_at,
+            updated_at=created_cheatsheet.updated_at,
+            is_public=creation_data.is_public,
+            tags=creation_data.tags,
+            count_like=0,
+            count_view=0,
+        )
+
+        assert created_cheatsheet == expected_result
