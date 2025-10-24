19a9361d71d7fc9b198e7437640a7431257094f6
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-10-24 22:54:18 +0500

    test: fix test_create_cheatsheet_success in cheatsheet use case

--- src/tests/unit/test_use_cases.py
+++ src/tests/unit/test_use_cases.py
@@ -25,7 +25,13 @@ class FakeCheatsheetRepo(ICheatsheetRepo):
         )
 
     async def create(self, cheatsheet: Cheatsheet) -> Cheatsheet:
-        return cheatsheet
+        updated_data = {
+            "cheatsheet_id": UUID("01998b2f-af53-7ca0-85f3-9c01093dd430"),
+            "created_at": datetime(2025, 1, 1),
+            "updated_at": datetime(2025, 1, 1),
+        }
+        created_cheatsheet = cheatsheet.update(updated_data)
+        return created_cheatsheet
 
 
 class FakeUnitOfWork(IUnitOfWork):
@@ -65,30 +71,24 @@ class TestCheatsheetUseCase:
     async def test_create_cheatsheet_success(self):
         unit_of_work = FakeUnitOfWork()
         sut = CheatsheetUseCase(unit_of_work)
+
         creation_data = Cheatsheet(
-            cheatsheet_id=UUID("01998b2f-af53-7ca0-85f3-9c01093dd430"),
             title="title",
             content="content",
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
             is_public=True,
             tags={Tag(1, "Python"), Tag(2, "Testing")},
             count_like=0,
             count_view=0,
         )
 
-        created_cheatsheet = await sut.create(creation_data)
+        generated_fields = {
+            "cheatsheet_id": UUID("01998b2f-af53-7ca0-85f3-9c01093dd430"),
+            "created_at": datetime(2025, 1, 1),
+            "updated_at": datetime(2025, 1, 1),
+        }
 
-        expected_result = Cheatsheet(
-            cheatsheet_id=created_cheatsheet.cheatsheet_id,
-            title=creation_data.title,
-            content=creation_data.content,
-            created_at=created_cheatsheet.created_at,
-            updated_at=created_cheatsheet.updated_at,
-            is_public=creation_data.is_public,
-            tags=creation_data.tags,
-            count_like=0,
-            count_view=0,
-        )
+        expected_result = creation_data.update(generated_fields)
+
+        created_cheatsheet = await sut.create(creation_data)
 
         assert created_cheatsheet == expected_result
