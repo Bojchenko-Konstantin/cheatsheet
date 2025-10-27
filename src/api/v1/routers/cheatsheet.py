57071102cad8df180eb3957a59e4c4f8125704cd
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-10-27 21:05:56 +0500

    feat: add status code in router

--- src/api/v1/routers/cheatsheet.py
+++ src/api/v1/routers/cheatsheet.py
@@ -32,7 +32,7 @@ async def get_cheatsheet_by_id(
     return cheatsheet
 
 
-@router.post("/", response_model=CheatsheetRead)
+@router.post("/", response_model=CheatsheetRead, status_code=status.HTTP_201_CREATED)
 async def create_cheatsheet(
     cheatsheet_data: CheatsheetCreate,
     cheatsheet_use_case: Annotated[CheatsheetUseCase, Depends(get_cheatsheet_use_case)],
