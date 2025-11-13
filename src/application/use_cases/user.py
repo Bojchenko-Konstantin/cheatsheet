d50052978286dccc7221223e7484e235f2e15e19
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-11-13 13:43:07 +0400

    feat: add UserUseCase

---
+++ src/application/use_cases/user.py
@@ -0,0 +1,11 @@
+from src.application.interfaces import IUnitOfWork
+
+
+class UserUseCase:
+    def __init__(self, unit_of_work: IUnitOfWork):
+        self._unit_of_work = unit_of_work
+
+    async def get_by_user_name(self, user_name: str) -> dict:
+        async with self._unit_of_work as uow:
+            user = await uow.user_repo.get_by_user_name(user_name)
+            return user
