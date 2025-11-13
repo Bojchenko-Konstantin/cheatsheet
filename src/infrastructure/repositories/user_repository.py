3a49fb49d04e13ff5997fb7e03744ad4a0548740
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-11-13 15:18:03 +0400

    fix: naming in UserRepo

--- src/infrastructure/repositories/user_repository.py
+++ src/infrastructure/repositories/user_repository.py
@@ -4,13 +4,14 @@ from typing import Any
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from infrastructure.repositories.utils import DictBundle
+from src.application.interfaces.repositories.user import IUserRepo
 from src.infrastructure.database.models import (
     UserModel,
 )
+from src.infrastructure.repositories.utils import DictBundle
 
 
-class SQLAlchemyUserRepo:
+class SQLAlchemyUserRepo(IUserRepo):
     """
     Class for operations with users that interact with database using SQLAlchemy.
     """
@@ -21,31 +22,31 @@ class SQLAlchemyUserRepo:
     def _to_model(self, user: MutableMapping[str, Any]) -> UserModel:
         pass
 
-    async def get_by_id(self, user_name: str):
+    async def get_by_user_name(self, user_name: str):
         statement = (
             select(
                 DictBundle(
                     "user",
                     UserModel.user_id,
                     UserModel.user_name,
-                    UserModel.email,
                     UserModel.hashed_password,
                     UserModel.is_active,
                     UserModel.is_superuser,
                     UserModel.is_verified,
                 ),
             )
-            .join(UserModel.detail)
+            # .join(UserModel.detail)
             .where(UserModel.user_name == user_name)
         )
         result = await self._session.execute(statement)
         model = result.one_or_none()
+        print(model)
 
         if not model:
             raise
 
-        cheatsheet = dict(**model.user)
-        return cheatsheet
+        user = model.user
+        return user
 
     async def create(self, create_data: dict[str, Any]):
         pass
