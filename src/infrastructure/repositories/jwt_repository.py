150859dd31dc4528ca3b2829375f7beeb8c3f63c
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-11-22 12:15:50 +0400

    feat: invalidate old active token on adding new token

--- src/infrastructure/repositories/jwt_repository.py
+++ src/infrastructure/repositories/jwt_repository.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timezone
+from uuid import UUID
+
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.application.dto import RefreshToken
@@ -10,6 +14,8 @@ class SQLAlchemyJWTRepo(IJWTRepo):
         self._session = session
 
     async def save(self, refresh_token: RefreshToken) -> None:
+        await self._revoke_older_refresh_token(refresh_token.user_id)  # type: ignore
+
         model = RefreshTokenModel(
             user_id=refresh_token.user_id,
             hashed_token=refresh_token.token_hash,
@@ -17,3 +23,18 @@ class SQLAlchemyJWTRepo(IJWTRepo):
             expires_at=refresh_token.expires_at,
         )
         self._session.add(model)
+
+    async def _revoke_older_refresh_token(self, user_id: UUID):
+        update_statement = (
+            update(RefreshTokenModel)
+            .where(
+                RefreshTokenModel.user_id == user_id, RefreshTokenModel.status_id == 1
+            )
+            .values(status_id=2, revoked_at=datetime.now(tz=timezone.utc))
+        )
+
+        try:
+            await self._session.execute(update_statement)
+
+        except Exception:
+            raise
