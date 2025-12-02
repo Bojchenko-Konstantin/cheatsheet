6bca2b9959f70d083a360c743a45f46de19ab717
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-12-02 12:14:06 +0400

    feat: add get_device_token_family method to IJWTRepo interface

--- src/application/interfaces/repositories/jwt.py
+++ src/application/interfaces/repositories/jwt.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 from src.application.dto import RefreshToken
 
@@ -8,4 +9,10 @@ class IJWTRepo(ABC):
 
     @abstractmethod
     async def save(self, refresh_token: RefreshToken) -> None:
+        pass
+
+    @abstractmethod
+    async def get_device_token_family(
+        self, user_id: UUID, fingerprint: str
+    ) -> list[RefreshToken]:
         pass
