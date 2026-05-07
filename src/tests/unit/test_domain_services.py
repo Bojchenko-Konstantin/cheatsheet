d00916e3bc8ffe4fb32bd4bf0810917b3ac963ff
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2026-05-07 21:26:11 +0400

    test: remove test for Cheatsheet update method

--- src/tests/unit/test_domain_services.py
+++ src/tests/unit/test_domain_services.py
@@ -17,43 +17,6 @@ OWNER_ID = UUID("019b4a71-173e-7f64-a840-9e8b042658cd")
 STRANGER_ID = UUID("019b4a71-173e-7f64-a840-9e8b042658ce")
 
 
-def test_cheatsheet_partial_update_was_successful():
-    original_cheatsheet = Cheatsheet(
-        cheatsheet_id=UUID("01998b2f-af53-7ca0-85f3-9c01093dd430"),
-        user_id=UUID("019b4a71-173e-7f64-a840-9e8b042658cd"),
-        title="Original Title",
-        content="Original Content",
-        is_public=True,
-        tags={Tag(1, "Python"), Tag(2, "Testing")},
-        created_at=datetime.now(),
-        updated_at=datetime.now(),
-        stats=CheatsheetStats(count_like=5, count_view=10),
-    )
-
-    update_fields = {
-        "title": "Updated Title",
-        "content": "Updated Content",
-        "is_public": False,
-        "tags": {Tag(3, "Algorithms")},
-    }
-
-    expected_result = Cheatsheet(
-        cheatsheet_id=original_cheatsheet.cheatsheet_id,
-        user_id=UUID("019b4a71-173e-7f64-a840-9e8b042658cd"),
-        title="Updated Title",
-        content="Updated Content",
-        is_public=False,
-        tags={Tag(3, "Algorithms")},
-        created_at=original_cheatsheet.created_at,
-        updated_at=original_cheatsheet.updated_at,
-        stats=CheatsheetStats(count_like=5, count_view=10),
-    )
-
-    updated_cheatsheet = original_cheatsheet.update(**update_fields)
-
-    assert updated_cheatsheet == expected_result
-
-
 def test_cheatsheet_creation_from_dict_was_successful():
     raw_tags_data = [
         {"tag_id": 1, "tag_name": "Python"},
