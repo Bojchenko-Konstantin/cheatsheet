f27b1b1aaaa5c7639b2a24beeac008b3da426760
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2026-05-09 21:01:27 +0500

    feat: update CheatsheetRepo with list and suggestions methods

--- src/application/interfaces/repositories/cheatsheet.py
+++ src/application/interfaces/repositories/cheatsheet.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any
 from uuid import UUID
 
+from src.application.dto import CheatsheetList, CheatsheetSearchSuggestions
 from src.domain.entities import Cheatsheet
 
 
@@ -18,4 +19,25 @@ class ICheatsheetRepo(ABC):
 
     @abstractmethod
     async def update(self, update_data: dict[str, Any]) -> Cheatsheet:
+        pass
+
+    @abstractmethod
+    async def list_accessible_cheatsheets(
+        self,
+        user_id: UUID | None,
+        cursor: str | None,
+        size: int,
+        tag: str | None,
+        search: str | None,
+        sort_by: str,
+        sort_order: str,
+    ) -> CheatsheetList:
+        pass
+
+    @abstractmethod
+    async def search_suggestions(
+        self,
+        query: str,
+        limit: int,
+    ) -> CheatsheetSearchSuggestions:
         pass
