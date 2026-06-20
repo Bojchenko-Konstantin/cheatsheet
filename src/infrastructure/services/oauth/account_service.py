417555689dabe5ea8090f69a2012f2219d39e41a
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2026-06-20 18:59:49 +0400

    feat: return user_id in save_account_with_refresh_token method

--- src/infrastructure/services/oauth/account_service.py
+++ src/infrastructure/services/oauth/account_service.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from cryptography.fernet import Fernet
 
 from src.core.config import settings
@@ -10,19 +12,21 @@ class OAuthAccountService:
 
     def __init__(
         self,
-        unit_of_work=SQLAlchemyUnitOfWork(),
+        unit_of_work: SQLAlchemyUnitOfWork = SQLAlchemyUnitOfWork(),
     ):
         self._unit_of_work = unit_of_work
         self._cipher = Fernet(settings.general_oauth.refresh_token_encryption_key)
 
     async def save_account_with_refresh_token(
         self, plain_refresh_token: str, user_info: dict[str, str]
-    ) -> None:
+    ) -> UUID:
         refresh_token_hash = self._generate_refresh_token_hash(plain_refresh_token)
         save_data = self._get_data_to_save(user_info)
 
         async with self._unit_of_work as uow:
-            await uow.oauth_repo.save(refresh_token_hash, save_data)
+            user_id = await uow.oauth_repo.save(refresh_token_hash, save_data)
+
+        return user_id
 
     def _generate_refresh_token_hash(self, plain_refresh_token: str) -> str:
         return self._cipher.encrypt(plain_refresh_token.encode()).decode()
